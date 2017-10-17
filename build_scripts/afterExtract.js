2f92ddc7bd4afba55be186abc6ede13401d0ad7d
Dominic Mooney <dominic_mooney67@hotmail.com>
2017-10-17 04:28:44 +0100

    Tidy script

--- build_scripts/afterExtract.js
+++ build_scripts/afterExtract.js
@@ -3,9 +3,6 @@ module.exports = function (extractPath, electronVersion, platform, arch, done) {
     const fs = require('fs');
 
     if (platform == 'win32') {
-        console.log('+++++Platform is ' + platform);        
-        //console.log(archive.pointer() + ' total bytes');
-        //console.log('archiver has been finalized and the output file descriptor has closed.');
         fs.createReadStream('./dll_bundle/'+arch+'/rtlsdr.dll').pipe(fs.createWriteStream(extractPath + '/rtlsdr.dll'));
         fs.createReadStream('./dll_bundle/'+arch+'/libFLAC_dynamic.dll').pipe(fs.createWriteStream(extractPath + '/libFLAC_dynamic.dll'));
         fs.createReadStream('./dll_bundle/'+arch+'/libFLAC++_dynamic.dll').pipe(fs.createWriteStream(extractPath + '/libFLAC++_dynamic.dll'));
