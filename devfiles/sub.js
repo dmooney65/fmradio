e610fd04ddc21eacd8e27e875e27cf3c47b0da28
Dominic Mooney <dominic.mooney99@gmail.com>
2017-10-20 06:58:31 +0100

    Remove unneeded files

--- devfiles/sub.js
+++
@@ -1,5 +0,0 @@
-process.on('message', (m) => {
-  console.log('CHILD got message:', m);
-});
-
-process.send({ foo: 'bar' });
--- devfiles/test_dlnacasts.js
+++
@@ -1,8 +0,0 @@
-var dlnacasts = require('dlnacasts')()
-
-dlnacasts.on('update', function (player) {
-  console.log('all players: ', dlnacasts.players)
-  player.play('http://commondatastorage.googleapis.com/gtv-videos-bucket/big_buck_bunny_1080p.mp4', {title: 'my video', type: 'video/mp4'})
-})
-
-dlnacasts.update();
